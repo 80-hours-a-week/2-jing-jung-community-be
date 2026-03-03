e8f90d3714b5e4f804407a1dc09f10bd7cceda69
jing-jung username <yhn5729@skuniv.ac.kr>
2026-03-03 14:26:46 +0900

    feat: complete community backend core features

--- app/main.py
+++ app/main.py
@@ -8,12 +8,7 @@ import os
 
 app = FastAPI()
 
-origins = [
-    "127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:8000",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
