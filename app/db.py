3a7d46a8087bfc2b612d13c25ff831999e103cb1
jing-jung username <yhn5729@skuniv.ac.kr>
2026-03-27 11:11:04 +0900

    eks에 맞게 수정

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # 1. .env 파일 로드
-load_dotenv()
+# load_dotenv()
 
 # 2. 환경 변수에서 값 꺼내기
 user = os.getenv("DB_USER")
@@ -16,7 +16,10 @@ db_name = os.getenv("DB_NAME")
 # 3. URL 조합하기
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True  # 연결이 끊겼는지 확인 후 다시 연결하는 옵션
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def get_db():
